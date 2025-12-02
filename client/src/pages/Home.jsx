cab88a53a538a68718e1d8dca1c3345118659bc5
JulianSellanes <julian.sellanes@gmail.com>
2025-12-02 20:49:52 -0300

    Updated Nav

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -14,8 +14,7 @@ export const Home = ({ user, projects = [] }) => {
                 <div className="home-content">
                     <h1 className="generic-title">Welcome to my Portfolio</h1>
                     {user && <p className="generic-p">I hope you have a good day, {user.username}!</p>}
-                    {/* <p className="generic-p">Mission: My mission is to continuously learn and grow as a software engineer, work with dedication, and save diligently, so that one day I can build and lead my own company! :)</p> */}
-                    <p className="generic-p">Mission: TEST NEW MISSION</p>
+                    <p className="generic-p">Mission: My mission is to continuously learn and grow as a software engineer, work with dedication, and save diligently, so that one day I can build and lead my own company! :)</p>
                     
                     <div className="home-bttns-group">
                         <Link className="generic-green-box green-bttn" to="/about">About Me</Link>
